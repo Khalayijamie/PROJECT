8d37d2559428900601e43e10f92a4e7803b9cd9d
Khalayijamie <94774663+Khalayijamie@users.noreply.github.com>
2023-11-29 05:33:22 +0300

    Update MPESAResponsesController.php

--- app/Http/Controllers/MPESAResponsesController.php
+++ app/Http/Controllers/MPESAResponsesController.php
@@ -1,20 +1,78 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use Carbon\Carbon;
 
-class MPESAResponsesController extends Controller
+class mpesaController extends Controller
 {
-    public function validation(Request $request){
-        Log::info('Validation endpoint hit');
-        Log::info($request->all());
+    public function generateAccessToken(){
+        $consumer_key ="C2o7O5GCPKa68UGRlG9OjnjfFxwXPPvn";
+        $consumer_secret = "mUm48VRFhEoeTQG1";
+        $credentials = base64_encode($consumer_key.":".$consumer_secret);
+        
+        $url = 'https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
+  
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Authorization: Basic '.$credentials)); //setting a custom header
+        curl_setopt($curl, CURLOPT_HEADER, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+  
+        $curl_response = curl_exec($curl);
+  
+        $access_token = json_decode($curl_response);
+
+        return $access_token->access_token;
     }
-    
-    public function confirmation(Request $request){
-        Log::info('Confirmation endpoint hit');
-        Log::info($request->all());
+
+    Public function STKPush(){
+        $BusinessShortCode = 174379;
+        $passkey ='bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919';
+        $timestamp= Carbon::rawParse('now')->format('YmdHms');
+
+        $password = base64_encode($BusinessShortCode.$passkey.$timestamp);
+        $Amount= 1;
+        $PartyA = 254769986556;
+        $PartyB = 174379;
+
+
+        $url = 'https://sandbox.safaricom.co.ke/mpesa/stkpush/v1/processrequest';
+  
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        $accessToken = $this->generateAccessToken();
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Authorization: Bearer ' . $accessToken));
+        
+        
+        
+        $curl_post_data = array(
+          //Fill in the request parameters with valid values
+          'BusinessShortCode' => $BusinessShortCode,
+          'Password' => $password,
+          'Timestamp' => $timestamp,
+          'TransactionType' => 'CustomerPayBillOnline',
+          'Amount' => $Amount,
+          'PartyA' => $PartyA,
+          'PartyB' => $PartyB,
+          'PhoneNumber' => $PartyA,
+          'CallBackURL' => ' https://611a-41-90-186-14.ngrok.io -> http://localhost:8000 /larproj/',
+          'AccountReference' => 'CodeXcellent Education ',
+          'TransactionDesc' => 'Testing stkpush on Sandbox '
+        );
+        
+        $data_string = json_encode($curl_post_data);
+        
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
+        
+        $curl_response = curl_exec($curl);
+        
+        
+        return $curl_response;
     }
-    
-}
